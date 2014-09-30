315bbc73e3f3424853add4660beae8f2305a1b7c
Maaz Asif <maazasif2@gmail.com>
2014-09-30 13:00:03 -0400

    Commit 1

---
+++ manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    manage
+    ~~~~~~
+
+    manage the project blah blah
+"""
+
+from flask.ext.script import Manager
+from flask.ext.assets import ManageAssets
+
+from qgic import app
+
+manager = Manager(app)
+
+@manager.command
+def runserver():
+    from os import environ as env
+    app.run(debug=True, host='0.0.0.0', port=int(env.get('PORT', '5000')))
+
+@manager.command
+def runnodebug():
+    from os import environ as env
+    app.run(host='0.0.0.0', port=int(env.get('PORT', '5000')))
+
+if __name__ == '__main__':
+    manager.run()
